63236a3d320fda2329fca2b47648cd8775e59c0d
Sagar Shinde <96124351+TrickyNoodle@users.noreply.github.com>
2024-11-20 20:58:12 +0530

    Add files via upload

---
+++ start_something.sh
@@ -0,0 +1,11 @@
+export GPU_MAX_HEAP_SIZE=100
+export GPU_MAX_USE_SYNC_OBJECTS=1
+export GPU_SINGLE_ALLOC_PERCENT=100
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_MAX_SINGLE_ALLOC_PERCENT=100
+export GPU_ENABLE_LARGE_ALLOCATION=100
+export GPU_MAX_WORKGROUP_SIZE=1024
+#!/bin/sh
+reset
+
+./SRBMiner-MULTI --algorithm verushash\;yespowerurx --pool stratum+tcp://ap.luckpool.net:3960\;stratum+tcp://yespowerURX.na.mine.zpool.ca:6236 --wallet R9NaS9xbLccaymcexgasoCb8E6jwGiHH4h\;ltc1qlplcx57ndmearkn4dlh4kr73w0p8yw8kvgf9rq --password x\;c=LTC --cpu-threads 0\;0 
